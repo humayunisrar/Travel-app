d28351a158d63823a73004900f34cd488711084a
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-02-08 17:03:38 +0500

    Hero.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+
 export default function Home() {
-  return (
-<h1 className="font-bold text-teal-500 text-2xl"> Hello World</h1>
-  );
-}
+  return(
+    
+    <>
+    <Hero />
+    <Camp/>
+    <Guide/>
+    <Features/>
+    <GetApp/>
+
+    
+    </>
+
+
+  )}
