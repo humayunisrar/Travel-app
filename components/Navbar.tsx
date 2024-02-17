b61e4f4ece8ebaa63569838f8ea7cc7c16481af5
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-02-17 13:53:39 +0500

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,33 +14,43 @@ const Navbar = () => {
 
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
-      <div onClick={() => {window.location.href = '/';}} style={{ cursor: 'pointer' }}>
+      <div onClick={toggleMenu} style={{ cursor: 'pointer' }}>
+      <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
       </div>
-      <ul className={`hidden h-full gap-12 lg:flex ${isMenuOpen ? 'block' : 'hidden'}`}>
+      <ul className={`lg:hidden absolute bg-white top-full right-0 w-1/3 ${isMenuOpen ? 'block' : 'hidden'} shadow-lg rounded-lg border border-gray-200 z-40` }>
         {NAV_LINKS.map((link) => (
-          <li key={link.key}>
-            <Link legacyBehavior href={link.href}>
-              <a className="regular-16 text-gray-50 cursor-pointer flexCenter pb-1.5 transition-all hover:font-bold">
+          <li key={link.key} className="border-b border-gray-200">
+            <Link href={link.href}>
+              <div className="block py-3 px-6 text-gray-700 font-semibold hover:bg-gray-100">
                 {link.label}
-              </a>
+              </div>
             </Link>
           </li>
         ))}
+      </ul>
+
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 cursor-pointer flexCenter pb-1.5 transition-all hover:font-bold "
+          >
+            {link.label}
+          </Link>
+        ))}
+
       </ul>
 
       <div className="lg:flexCenter hidden">
         <Button type="button" title="Log In" icon="/user.svg" variant="btn_dark_green" />
       </div>
 
-      <Image
-        src="/menu.svg"
-        alt="menu"
-        width={32}
-        height={32}
-        className="lg:hidden cursor-pointer inline-block"
-        onClick={toggleMenu}
-      />
+      <div onClick={toggleMenu} className="lg:hidden cursor-pointer inline-block">
+        <Image src="/menu.svg" alt="menu" width={32} height={32} />
+      </div>
     </nav>
   );
 };
