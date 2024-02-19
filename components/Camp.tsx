a95a7c39f3cea0d5ee7072cce42a3ad39b41aa85
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-02-19 19:38:01 +0500

    camp.tsx

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
+const CampSite = () => {
+
+
+  
+}
+
 const Camp = () => {
   return (
-    <div>Camp</div>
+    <section className='border-2 border-green 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
+        <div className='hide-scrollbar flex h-[340px] w-full items-start justify-start overflow-x-auto gap-8 lg:h-[400px] xl:[640px]'>
+        
+        </div>
+    </section>
   )
 }
 
